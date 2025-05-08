0c4e30e27b8df1d5b742100c0a68459d90045282
Duma29 <al046879@uacam.mx>
2025-05-08 16:28:47 -0600

    perdido

---
+++ Practica 4/src/main/java/utils/JwtUtil.java
@@ -0,0 +1,24 @@
+import io.jsonwebtoken.*;
+import java.util.Date;
+
+public class JwtUtil {
+    private static final String SECRET_KEY = "mi_clave_secreta";
+    private static final long EXPIRATION_TIME = 300000; // 5 minutos
+
+    public static String generateToken(String username) {
+        return Jwts.builder()
+                .setSubject(username)
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+                .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
+                .compact();
+    }
+
+    public static boolean validateToken(String token) {
+        try {
+            Jwts.parser().setSigningKey(SECRET_KEY).parseClaimsJws(token);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+}
